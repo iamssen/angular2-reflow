12991b7d35fe6e6e7cad17fd07b4dee5c20fcd06
SSen <i@ssen.name>
2016-01-26 04:06:57 +0900

    fix EventBus interface bug

--- src/reflow.core.ts
+++ src/reflow.core.ts
@@ -17,8 +17,8 @@ export interface EventListener {
 export interface EventBus {
   addEventListener(eventType:string, listener:(event) => void):EventListener;
   on(eventType:string, listener:(event) => void):EventListener;
-  dispatchEvent(event, toGlobal:boolean);
-  fire(event, toGlobal:boolean);
+  dispatchEvent(event, toGlobal?:boolean);
+  fire(event, toGlobal?:boolean);
 }
 
 export interface Command {
